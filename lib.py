5650728f06e7e7ff9262cfa373da9e35d2e90832
ph4s3r <peterkaracsonyi85@gmail.com>
2023-12-14 02:59:07 +0100

    move 2 funcs to a separate file

---
+++ lib.py
@@ -0,0 +1,49 @@
+import matplotlib as plt
+import torch
+from torchvision.models import resnet18
+
+# plot a batch of tiles with masks
+def vizBatch(im, tile_masks, tile_labels = None):
+    # create a grid of subplots
+    _, axes = plt.subplots(4, 4, figsize=(8, 8))  # Adjust figsize as needed
+    axes = axes.flatten()
+
+    for i in range(8):  # Display only the first 8 tiles, duplicated
+        img = im[i].permute(1, 2, 0).numpy()
+        mask = tile_masks[i].permute(1, 2, 0).numpy()
+
+        # Display image without mask
+        axes[2*i].imshow(img)
+        axes[2*i].axis("off")
+        if tile_labels is not None:
+            label = ", ".join([f"{v[i]}" for _, v in tile_labels.items()])
+        axes[2*i].text(3, 10, label, color="white", fontsize=6, backgroundcolor="black")
+
+        # Display image with mask overlay
+        axes[2*i + 1].imshow(img)
+        axes[2*i + 1].imshow(mask, alpha=1, cmap='terrain')  # adjust alpha as needed
+        axes[2*i + 1].axis("off")
+
+    plt.tight_layout()
+    plt.show()
+
+def test_model(test_loader, model_path, device):
+
+    model = resnet18()
+    model.load_state_dict(torch.load(model_path))
+    model = model.to(device)
+    model.eval()
+
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy: {:.2f}%'.format(100 * correct / total))
