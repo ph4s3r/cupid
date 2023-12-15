fae2570b31165a0b2b067972e2631843dff2a1f4
ph4s3r <peterkaracsonyi85@gmail.com>
2023-12-15 13:58:54 +0100

    add roc/auc plot

--- lib.py
+++ lib.py
@@ -1,5 +1,6 @@
-import matplotlib as plt
 import torch
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
 
 # plot a batch of tiles with masks
 def vizBatch(im, tile_masks, tile_labels = None):
@@ -30,6 +31,10 @@ def test_model(test_loader, model_path, device, model):
 
     model.load_state_dict(torch.load(model_path))
     model = model.to(device)
+
+    true_labels = []
+    predictions = []
+
     model.eval()
 
     correct = 0
@@ -41,7 +46,29 @@ def test_model(test_loader, model_path, device, model):
             labels = labels.to(device)
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
+            outputs.shape
+            outputs.data.shape
+            probabilities = torch.sigmoid(outputs)  # the senet does not have a sigmoid output
+            predictions.extend(probabilities[:, 1].cpu().numpy()) # getting back the probs for both class, roc_curve needs only the positive 
+            true_labels.extend(labels.cpu().numpy())
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
     print('Accuracy: {:.2f}%'.format(100 * correct / total))
+
+    fpr, tpr, _ = roc_curve(true_labels, predictions)
+    roc_auc = auc(fpr, tpr)
+
+    # plot roc / auc
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
+
+    
