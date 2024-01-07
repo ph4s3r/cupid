43d2ba0918471dcc8fac83be79b0495809390666
peterkaracsonyi <peterkaracsonyi85@gmail.com>
2024-01-07 06:18:56 +0100

    save full model state on keyboardinterrupt / per epoch

--- PyTorch-Trainer.py
+++ PyTorch-Trainer.py
@@ -15,24 +15,25 @@ if os.name == "nt":
 import helpers.ds_means_stds
 import lib
 # pip
+import sys
 import time
-import numpy as np
-import pathml
 import torch
+import pathml
+import signal
+import numpy as np
 from pathlib import Path
-from coolname import generate_slug # ...
+from coolname import generate_slug
 from sklearn.metrics import precision_recall_fscore_support
 from torch.optim.lr_scheduler import MultiStepLR
+from torchvision.transforms import v2
+
 from torch.utils.tensorboard import SummaryWriter # launch with http://localhost:6006/
-from torchvision.transforms import (
-    v2,
-)  # v2 is the newest: https://pytorch.org/vision/stable/transforms.html
 
 #####################
 # configure folders #
 #####################
 base_dir = Path("/mnt/bigdata/placenta")
-h5folder = base_dir / Path("h5")
+h5folder = base_dir / Path("h5mini")
 h5files = list(h5folder.glob("*.h5path"))
 model_checkpoint_dir = base_dir / Path("training_checkpoints")
 result_path = base_dir / Path("training_results")
@@ -123,6 +124,30 @@ class TransformedPathmlTileSet(pathml.ml.TileDataset):
         return (tile_image, tile_masks, tile_labels, slide_labels)
 
 
+############################################$###########
+# KeyboardInterrupt: stop training and save checkpoint #
+#############################################$$#########
+
+# global KeyboardInterrupt Flag
+global interrupted
+interrupted = False
+# signal handler
+def signal_handler(signum, frame):
+    global interrupted
+    interrupted = True
+    print("Interrupt received, stopping...")
+# attach signal handler
+signal.signal(signal.SIGINT, signal_handler)
+
+def save_model(epoch, model, optimizer, scheduler, checkpoint_file):
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'scheduler_state_dict': scheduler.state_dict(),
+            }, checkpoint_file)
+        print(f"Model successfully saved to file {checkpoint_file}")
+
 #########################################################################
 # inserting tiles from h5path with TransformedPathmlTileSet to datasets #
 #########################################################################
@@ -151,7 +176,7 @@ if determine_global_std_and_means:
 ######################
 # set up dataloaders #
 ######################
-batch_size = 128 # larger batch is faster!
+batch_size = 64 # larger batch is faster! (se_resnet max batch on 4080 with 16G mem is 64. on 3080 it is 32)
 # fixed generator for reproducible split results
 generator = torch.Generator().manual_seed(42)
 train_cases, val_cases = torch.utils.data.random_split( # split to 70% train, 20% val
@@ -255,124 +280,110 @@ early_stop_val_loss = EarlyStopping(
     verbose=True,
     consecutive=False
 )
-try: 
-    for epoch in range(num_epochs):
 
-        model.train()
+for epoch in range(num_epochs):
+    model.train()
 
-        total_loss = 0
-        total_correct = 0
-        total = 0
-        all_labels = []
-        all_predictions = []
+    total_loss = 0
+    total_correct = 0
+    total = 0
+    all_labels = []
+    all_predictions = []
 
-        for i, (images, _, labels_dict, _) in enumerate(train_loader):
+    for i, (images, _, labels_dict, _) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels_dict['class'].to(device)
+        # forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        total_correct += (predicted == labels).sum().item()
+        all_labels.extend(labels.cpu().numpy())
+        all_predictions.extend(predicted.cpu().numpy())
+        
+        if (i+1) % 100 == 0:
+            time_elapsed = time.time() - start_time
+            print ("Training   - Epoch [{}/{}], Step [{}/{}] Loss: {:.4f} in {:.0f}m {:.0f}s"
+                .format(epoch+1, num_epochs, i+1, total_step, loss.item(), time_elapsed // 60, time_elapsed % 60))
+
+    epoch_loss = total_loss / total_step
+    epoch_acc = total_correct / total
+    # https://scikit-learn.org/0.15/modules/generated/sklearn.metrics.precision_recall_fscore_support.html
+    precision, recall, f1_score, _ = precision_recall_fscore_support(all_labels, all_predictions, labels=[0,1], average='weighted')
+
+    # write epoch learning stats to tensorboard & file
+    writer.add_scalar("loss/train", loss, epoch)
+    writer.add_scalar('accuracy/train', epoch_acc, epoch)
+    writer.add_scalar('weighted_precision/train', precision, epoch)
+    writer.add_scalar('weighted_recall/train', recall, epoch)
+    writer.add_scalar('weighted_f1/train', f1_score, epoch)
+    latest_lr = torch.optim.lr_scheduler.MultiStepLR.get_last_lr(scheduler)[-1]
+    writer.add_scalar('learning_rate', latest_lr, epoch)
+
+    # show stats at the end of epoch
+    print(f"Epoch {epoch+1}/{num_epochs} - Loss: {epoch_loss:.4f}, Acc: {epoch_acc:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1_score:.4f}, Last lr: {latest_lr:.4f}")
+
+    # validation
+    model.eval()
+
+    val_loss = 0
+    val_correct = 0
+    val_total = 0
+    val_all_labels = []
+    val_all_predictions = []
+
+    with torch.no_grad():
+        for images, _, labels_dict, _ in val_loader:
             images = images.to(device)
             labels = labels_dict['class'].to(device)
-            # forward pass
+
             outputs = model(images)
             loss = criterion(outputs, labels)
 
-            # backward and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            total_loss += loss.item()
+            val_loss += loss.item()
             _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            total_correct += (predicted == labels).sum().item()
-            all_labels.extend(labels.cpu().numpy())
-            all_predictions.extend(predicted.cpu().numpy())
-            
-            if (i+1) % 100 == 0:
-                time_elapsed = time.time() - start_time
-                print ("Training   - Epoch [{}/{}], Step [{}/{}] Loss: {:.4f} in {:.0f}m {:.0f}s"
-                    .format(epoch+1, num_epochs, i+1, total_step, loss.item(), time_elapsed // 60, time_elapsed % 60))
+            val_total += labels.size(0)
+            val_correct += (predicted == labels).sum().item()
+            val_all_labels.extend(labels.cpu().numpy())
+            val_all_predictions.extend(predicted.cpu().numpy())
 
-        epoch_loss = total_loss / total_step
-        epoch_acc = total_correct / total
-        # https://scikit-learn.org/0.15/modules/generated/sklearn.metrics.precision_recall_fscore_support.html
-        precision, recall, f1_score, _ = precision_recall_fscore_support(all_labels, all_predictions, labels=[0,1], average='weighted')
+    val_epoch_loss = val_loss / len(val_loader)
+    val_epoch_acc = val_correct / val_total
+    val_precision, val_recall, val_f1_score, _ = precision_recall_fscore_support(val_all_labels, val_all_predictions, labels=[0,1], average='weighted')
 
-        # write epoch learning stats to tensorboard & file
-        writer.add_scalar("loss/train", loss, epoch)
-        writer.add_scalar('accuracy/train', epoch_acc, epoch)
-        writer.add_scalar('weighted_precision/train', precision, epoch)
-        writer.add_scalar('weighted_recall/train', recall, epoch)
-        writer.add_scalar('weighted_f1/train', f1_score, epoch)
-        latest_lr = torch.optim.lr_scheduler.MultiStepLR.get_last_lr(scheduler)[-1]
-        writer.add_scalar('learning_rate', latest_lr, epoch)
+    # log validation stats
+    writer.add_scalar("loss/val", val_epoch_loss, epoch)
+    writer.add_scalar('accuracy/val', val_epoch_acc, epoch)
+    writer.add_scalar('weighted_precision/val', val_precision, epoch)
+    writer.add_scalar('weighted_recall/val', val_recall, epoch)
+    writer.add_scalar('weighted_f1/val', val_f1_score, epoch)
 
-        # show stats at the end of epoch
-        print(f"Epoch {epoch+1}/{num_epochs} - Loss: {epoch_loss:.4f}, Acc: {epoch_acc:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1_score:.4f}, Last lr: {latest_lr:.4f}")
+    print(f"Validation - Epoch {epoch+1}/{num_epochs} - Loss: {val_epoch_loss:.4f}, Acc: {val_epoch_acc:.4f}, Precision: {val_precision:.4f}, Recall: {val_recall:.4f}, F1: {val_f1_score:.4f}")
 
-        # validation
-        model.eval()
+    # decay learning rate
+    scheduler.step()
 
-        val_loss = 0
-        val_correct = 0
-        val_total = 0
-        val_all_labels = []
-        val_all_predictions = []
+    # save model checkpoint and data (epoch)
+    if epoch > 2 or interrupted:
+        checkpoint_file = str(model_checkpoint_dir)+"/"+session_name+str(epoch)+".ckpt"
+        save_model(epoch, model, optimizer, scheduler, checkpoint_file)
 
-        with torch.no_grad():
-            for images, _, labels_dict, _ in val_loader:
-                images = images.to(device)
-                labels = labels_dict['class'].to(device)
+    if interrupted:
+        print(f"KeyboardInterrupt received: saving model for session {session_name} and exiting")
+        break
 
-                outputs = model(images)
-                loss = criterion(outputs, labels)
-
-                val_loss += loss.item()
-                _, predicted = torch.max(outputs.data, 1)
-                val_total += labels.size(0)
-                val_correct += (predicted == labels).sum().item()
-                val_all_labels.extend(labels.cpu().numpy())
-                val_all_predictions.extend(predicted.cpu().numpy())
-
-        val_epoch_loss = val_loss / len(val_loader)
-        val_epoch_acc = val_correct / val_total
-        val_precision, val_recall, val_f1_score, _ = precision_recall_fscore_support(val_all_labels, val_all_predictions, labels=[0,1], average='weighted')
-
-        # log validation stats
-        writer.add_scalar("loss/val", val_epoch_loss, epoch)
-        writer.add_scalar('accuracy/val', val_epoch_acc, epoch)
-        writer.add_scalar('weighted_precision/val', val_precision, epoch)
-        writer.add_scalar('weighted_recall/val', val_recall, epoch)
-        writer.add_scalar('weighted_f1/val', val_f1_score, epoch)
-
-        print(f"Validation - Epoch {epoch+1}/{num_epochs} - Loss: {val_epoch_loss:.4f}, Acc: {val_epoch_acc:.4f}, Precision: {val_precision:.4f}, Recall: {val_recall:.4f}, F1: {val_f1_score:.4f}")
-
-        # decay learning rate
-        scheduler.step()
-
-        # save model checkpoint and data (epoch)
-        if epoch > 2:
-            checkpoint_file = str(model_checkpoint_dir)+"/"+session_name+str(epoch)+".ckpt"
-            torch.save({
-                'epoch': epoch,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'scheduler_state_dict': scheduler.state_dict(),
-                }, checkpoint_file)
-
-        # check early stopping conditions, stop if necessary
-        if early_stop_val_loss(val_epoch_loss):
-            break
-
-        # end of epoch run (identation!)
-except KeyboardInterrupt:
-    print("Training interrupted by user. Exiting the loop.")
-    checkpoint_file = str(model_checkpoint_dir)+"/"+session_name+str(epoch)+".ckpt"
-    torch.save({
-        'epoch': epoch,
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-        'scheduler_state_dict': scheduler.state_dict(),
-        }, checkpoint_file)
-except Exception as e:
-        print(f"An error occurred during training: {e}. Continuing to evaluation.")
+    # check early stopping conditions, stop if necessary
+    if early_stop_val_loss(val_epoch_loss):
+        break
+    # end of epoch run (identation!)
 
 writer.flush()
 writer.close()
