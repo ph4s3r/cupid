20f7a5c4337ed93680b6a4e99e4de11d480c9632
peterkaracsonyi <peterkaracsonyi85@gmail.com>
2024-01-20 00:25:54 +0100

    skip already existing

--- pathML-TileSaver.py
+++ pathML-TileSaver.py
@@ -24,7 +24,7 @@ from torchvision.utils import save_image
 # where to load the h5s from #
 ##############################
 base_dir = "/mnt/bigdata/placenta"
-h5_subdir = "h5-mini"
+h5_subdir = "h5-train"
 
 
 ###########################
@@ -40,12 +40,23 @@ datasets = []
 ds_fullsize = 0
 h5folder = Path(base_dir) / Path(h5_subdir)
 h5files = list(h5folder.glob("*.h5path"))
+
+## check if tiles exist for a slide, then skip
+tile_dir = Path(base_dir) / Path(tile_dir)
+tile_dir.mkdir(parents=True, exist_ok=True)
+
+tiles_exist_for = [f.stem for f in tile_dir.iterdir() if f.is_dir()]
+
+
 for h5file in h5files:
-    ds_start_time = time.time()
-    print(f"creating dataset from {str(h5file)} started at {datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')}")
-    datasets.append(lib.TransformedPathmlTileSet(h5file))
-    ds_complete = time.time() - ds_start_time
-    print('dataset processed in {:.0f}m {:.0f}s'.format(ds_complete // 60, ds_complete % 60))
+    if h5file.stem not in tiles_exist_for:
+        ds_start_time = time.time()
+        print(f"creating dataset from {str(h5file)} started at {datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')}")
+        datasets.append(lib.TransformedPathmlTileSet(h5file))
+        ds_complete = time.time() - ds_start_time
+        print('dataset processed in {:.0f}m {:.0f}s'.format(ds_complete // 60, ds_complete % 60))
+    else:
+        print(f"skipping {h5file.stem} because tiles directory exists")
 
 for ds in datasets:
     ds_fullsize += ds.dataset_len
@@ -91,8 +102,6 @@ def save_tiles(dataloader, save_dir):
 ##############
 # save tiles #
 ##############
-tile_dir = Path(base_dir) / Path(tile_dir)
-tile_dir.mkdir(parents=True, exist_ok=True)
 st_start_time = time.time()
 save_tiles(dataloader, tile_dir)
 st_complete = time.time() - st_start_time
