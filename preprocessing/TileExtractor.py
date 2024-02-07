8af7ceafb36d9e6d22143f5b11336a84aaf68625
ph4s3r <peterkaracsonyi85@gmail.com>
2024-02-07 22:32:38 +0100

    config as dict, add tdhe as config, rename some stuff

--- preprocessing/TileExtractor.py
+++ preprocessing/TileExtractor.py
@@ -1,8 +1,9 @@
 ##########################################################################################################
 # Author: Peter Karacsonyi                                                                               #
-# Last updated: 2024 jan 23                                                                              #
-# Input: creates useful jpeg tiles (pathml mask) from a tiff wsi                                         #
+# Last updated: 2024 feb 7                                                                               #
+# extracts useful jpeg tiles (using pathml TissueDetectionHE mask) from a wsi                            #
 ##########################################################################################################
+
 
 ##############
 # IO folders #
@@ -14,9 +15,15 @@ out_folder = "/mnt/bigdata/echino/tiles"  # creates tiles in a directory with ws
 ##########
 # Config #
 ##########
-resolution = 0      # 0 is the highest wsi resolution level, for available resolutions see the index of level_downsamples (will be printed below)
-coverage   = 0.35   # percentage the mask should cover the tile to avoid getting dropped
-tile_size  = 500
+config = {
+    'resolution' : 0,      # 0 is the highest wsi resolution level, for available resolutions see the index of level_downsamples (will be printed below)
+    'coverage'   : 0.30,   # percentage the mask should cover the tile to avoid getting dropped
+    'tile_size'  : 500,
+    'TissueDetectionHE_threshold' : 19,
+    'TissueDetectionHE_min_region_size' : 1000,
+    'TissueDetectionHE_max_hole_size' : 10000,
+    'TissueDetectionHE_use_saturation' : True
+}
 
 
 ###########
@@ -42,16 +49,16 @@ colorama.init()
 # operations on a numpy.ndarray region: pathML TissueDetectionHE and PIL Image save #
 #####################################################################################
 def TissueDetectandSave(image_nparray, coverage, i):
-    im = Tile(image_nparray, coords=(0, 0), name="quartertile", slide_type=types.HE)
+    im = Tile(image_nparray, coords=(0, 0), name="quadrant", slide_type=types.HE)
 
     st = time.time()
     TissueDetectionHE(
         mask_name="tissue",
-        threshold=19,
-        min_region_size=1000,
+        threshold=config.get('TissueDetectionHE_threshold'),
+        min_region_size=config.get('TissueDetectionHE_min_region_size'),
         outer_contours_only=False,
-        max_hole_size=10000,
-        use_saturation=True,
+        max_hole_size=config.get('TissueDetectionHE_max_hole_size'),
+        use_saturation=config.get('TissueDetectionHE_use_saturation')
     ).apply(im)
     print(f"\tTissueDetectionHE [{i}] took {(time.time() - st) // 60:.0f}m {(time.time() - st) % 60:.0f}s")
 
@@ -59,8 +66,8 @@ def TissueDetectandSave(image_nparray, coverage, i):
     im.masks["tissue"][im.masks["tissue"] == 127] = 1  # convert 127s to 1s
     im.image *= np.expand_dims(im.masks["tissue"], 2)  # element wise in-place mm
 
-    maxx = ((im.shape[0] // tile_size) * tile_size) - tile_size
-    maxy = ((im.shape[1] // tile_size) * tile_size) - tile_size
+    maxx = ((im.shape[0] // config.get('tile_size')) * config.get('tile_size')) - config.get('tile_size')
+    maxy = ((im.shape[1] // config.get('tile_size')) * config.get('tile_size')) - config.get('tile_size')
 
     tilecounter = 0
     tile_x_max = 0
@@ -69,12 +76,12 @@ def TissueDetectandSave(image_nparray, coverage, i):
     wsiname = wsi.stem
     Path(out_folder + "/" + wsiname).mkdir(parents=True, exist_ok=True)
     # can be 250.000 maximum (500x500) x value of 1 where mask is covering the image
-    threshold = coverage * tile_size * tile_size
+    threshold = coverage * config.get('tile_size') * config.get('tile_size')
 
-    for tile_x_start in range(0, maxx, tile_size):
-        for tile_y_start in range(0, maxy, tile_size):
-            tile_x_end = tile_x_start + tile_size
-            tile_y_end = tile_y_start + tile_size
+    for tile_x_start in range(0, maxx, config.get('tile_size')):
+        for tile_y_start in range(0, maxy, config.get('tile_size')):
+            tile_x_end = tile_x_start + config.get('tile_size')
+            tile_y_end = tile_y_start + config.get('tile_size')
             tile_mask = im.masks["tissue"][
                 tile_x_start:tile_x_end, tile_y_start:tile_y_end
             ]
@@ -106,15 +113,15 @@ def processWSI(wsi):
     tiles_total = 0
     openslide_wsi = openslide.OpenSlide(wsi)
 
-    w = openslide_wsi.level_dimensions[resolution][1]
-    h = openslide_wsi.level_dimensions[resolution][0]
+    w = openslide_wsi.level_dimensions[config.get('resolution')][1]
+    h = openslide_wsi.level_dimensions[config.get('resolution')][0]
 
-    # first half of the pixels per dimension - better to be divisible by tile_size
-    x1 = ((w // tile_size) // 2) * tile_size
-    y1 = ((h // tile_size) // 2) * tile_size
+    # first half of the pixels per dimension - better to be divisible by config.get('tile_size')
+    x1 = ((w // config.get('tile_size')) // 2) * config.get('tile_size')
+    y1 = ((h // config.get('tile_size')) // 2) * config.get('tile_size')
     # another half of the pixels per dimension
-    x2 = int(w - x1) + tile_size
-    y2 = int(h - y1) + tile_size
+    x2 = int(w - x1) + config.get('tile_size')
+    y2 = int(h - y1) + config.get('tile_size')
 
     print("shape and resolution details of the input file: ")
     print("\tlevel_count: ", openslide_wsi.level_count)
@@ -130,7 +137,7 @@ def processWSI(wsi):
     ]
 
     for i in range(4):
-        print(f"processing quarter {i+1}")
+        print(f"processing quadrant {i+1}")
         # print(f"\tDEBUG             [{i}]: bounding boxes (loc_x, loc_y, size_x, size_y): {qregions[i]}")
         st = time.time()
         image_pil = openslide_wsi.read_region(
@@ -139,7 +146,7 @@ def processWSI(wsi):
                 qregions[i][2],
                 qregions[i][3],
             ),
-            level=resolution,
+            level=config.get('resolution'),
         )
         print(f"\tread_region()     [{i}] {(time.time() - st) // 60:.0f}m {(time.time() - st) % 60:.0f}s")
 
@@ -150,7 +157,7 @@ def processWSI(wsi):
         del image_pil
         print(f"\tnp.asarray()      [{i}] took {(time.time() - st) // 60:.0f}m {(time.time() - st) % 60:.0f}s")
         
-        tiles_total += TissueDetectandSave(image_nparray, coverage, i)
+        tiles_total += TissueDetectandSave(image_nparray, config.get('coverage'), i)
         del image_nparray
 
     openslide_wsi.close()
@@ -174,7 +181,7 @@ if int(psutil.swap_memory().total) < int(psutil.virtual_memory().available):
 wsi_paths = list(Path(wsi_folder).glob("*.tif*"))
 for wsi in wsi_paths:
     print(
-        f"************ Processing {wsi.name} on resolution level {resolution} ************"
+        f"************ Processing {wsi.name} on resolution level {config.get('resolution')} ************"
     )
     print("\r\n")
     start_time = time.time()
